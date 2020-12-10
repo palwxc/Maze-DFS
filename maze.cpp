7fd06081c3344edd6335fdde31292a6f6b83bd10
Phillip LaFrance <palwxc@mst.edu>
2020-12-09 19:02:30 -0600

    Typo in comments

--- maze.cpp
+++ maze.cpp
@@ -1,5 +1,5 @@
 //Programmer: Phillip LaFrance
-//Intructor: Patrick Taylor		//Section: A		//Date: 02/19/2018
+//Instructor: Patrick Taylor		//Section: A		//Date: 02/19/2018
 //File: maze.cpp  				//Description: "Takes in a user defined maze and attempts to solve it."
 
 #include "maze.h"
